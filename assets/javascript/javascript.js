e06c196ff3916e30b97b651d0bdcdf41ad883f4e
Urvish Patel <urvishp13@gmail.com>
2018-10-03 11:38:07 -0400

    added comments

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,17 +5,21 @@ function renderButtons() {
   // Emptying out the animals view div to prevent repeats
   $("#animals-view").empty();
 
-  // Looping through topics array to make the buttons of the animal
+  // Looping through topics array to make the buttons of the animals
   for (var i = 0; i < topics.length; i++) {
+    // create new buttons
     var newButton = $("<button>");
     newButton.addClass("animal-btn")
+    // give an attribute of the animal type
     newButton.attr("data-name", topics[i]);
     newButton.text(topics[i]);
     $("#animals-view").append(newButton);
   }
 }
 
+// add functionality on the buttons
 $(document).on("click", ".animal-btn", function () {
+  // url of the gifs for the animal
   var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=" + $(this).attr("data-name") + "&limit=5&rating=g";
 
   $.ajax({
@@ -32,6 +36,7 @@ $(document).on("click", ".animal-btn", function () {
 
       var animalImage = $("<img>");
       animalImage.addClass("gif")
+      // adding attributes to each image tag of animal
       animalImage.attr({
         "src": results[i].images.fixed_width_still.url,
         "data-still": results[i].images.fixed_width_still.url,
@@ -47,6 +52,7 @@ $(document).on("click", ".animal-btn", function () {
   })
 })
 
+// on click functionality on the gif, making the gif play
 $(document).on("click", ".gif", function () {
   var state = $(this).attr("data-state");
 
@@ -65,7 +71,9 @@ $(document).on("click", ".gif", function () {
   }
 });
 
+// adding on click functionality to submit a new animal to the buttons
 $("#add-animal").on("click", function (event) {
+  // preventing the form from refreshing
   event.preventDefault();
   var animal = $("#animal-input").val().trim();
   topics.push(animal);
